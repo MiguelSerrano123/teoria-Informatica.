2283a13768b2636761090f1b5b44f43c76bbdba1
Josemi10 <serrano.sajos18@zaragoza.salesianos.edu>
2025-09-16 11:50:03 +0200

    Add files via upload

---
+++ Suma.py
@@ -0,0 +1,8 @@
+def Suma():
+    #cadena=raw_input("Dime tu nombre: ") lee cadena como una cadena
+    #print("Bienvenido "+ cadena)
+    num1=input("Dime un numero:") #lee num1 como número
+    num2=input("Dime otro:") #lee num2 como número
+    print(str(num1)+" + "+str(num2)+ "=" +str(num1+num2)) #str convierte un numero en cadena
+
+Suma()
